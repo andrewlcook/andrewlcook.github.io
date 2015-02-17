99fc062713c9f2adac70a42e9e3327c6845159df
andrewlcook <andrewcook@gmail.com>
2015-02-17 14:49:17 -0330

    updated again

--- phant-vis.js
+++ phant-vis.js
@@ -45,6 +45,19 @@ $.ajax({
 		global: {
 			 useUTC: false
 		}
+		chart: {
+            backgroundColor: {
+                linearGradient: [0, 0, 500, 500],
+                stops: [
+                    [0, 'rgb(255, 255, 255)'],
+                    [1, 'rgb(240, 240, 255)']
+                    ]
+            },
+            borderWidth: 2,
+            plotBackgroundColor: 'rgba(255, 255, 255, .9)',
+            plotShadow: true,
+            plotBorderWidth: 1
+        }
 	  });
       var chart = chartHolder.highcharts({
         chart: {
@@ -59,7 +72,8 @@ $.ajax({
         },
         yAxis: {
           title: {
-            text: ''
+            text: '',
+			min: 0
           }
         },
         legend: {
